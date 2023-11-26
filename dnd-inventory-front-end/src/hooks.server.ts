10c2d9587af69edb0ea56591ec5eda71ae8e9aa4
Mats Daamen <56251011+MatsDaamen@users.noreply.github.com>
2023-11-26 22:15:30 +0100

    set up userdatabase to store user/google login connection

--- dnd-inventory-front-end/src/hooks.server.ts
+++ dnd-inventory-front-end/src/hooks.server.ts
@@ -2,6 +2,8 @@ import { SvelteKitAuth } from "@auth/sveltekit"
 import Google from "@auth/core/providers/google"
 import { sequence } from '@sveltejs/kit/hooks';
 import type { Handle } from '@sveltejs/kit';
+import { MongoDBAdapter } from "@auth/mongodb-adapter";
+import clientPromise from "$lib/database/clientPromise";
 
 
 export const defaultHandle: Handle = async ({ event, resolve }) => {
@@ -17,6 +19,9 @@ export const authHandle: Handle = SvelteKitAuth(async () => {
 				clientSecret: "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"//import.meta.env.VITE_GOOGLE_SECRET
 			})
 		],
+		adapter: MongoDBAdapter(clientPromise, {
+			databaseName: 'accounts'
+		}),
 		secret: "bd05789392e962f687ed468a66de0319",//import.meta.env.VITE_SECRET,
 		trustHost: true
 	};
