54cc983b6a606d634e8f3cf2d9df96751cce461b
Mats Daamen <56251011+MatsDaamen@users.noreply.github.com>
2024-01-02 16:25:42 +0100

    Create IInventoryService.cs

---
+++ Dnd Inventory/Dnd Inventory Logic/Interfaces/Services/IInventoryService.cs
@@ -0,0 +1,18 @@
+﻿using Dnd_Inventory_Logic.DomainModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dnd_Inventory_Logic.Interfaces.Services
+{
+    public interface IInventoryService
+    {
+        public InventoryModel GetAll(string userId);
+
+        public void AddItem(int itemId, string userId, int amount = 1);
+
+        public void RemoveItem(int itemId, string userId, int amount = 1);
+    }
+}
