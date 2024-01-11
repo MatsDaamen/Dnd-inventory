8fd374b8e2cb24b3d19aacb9552df598e33b3cce
Mats Daamen <56251011+MatsDaamen@users.noreply.github.com>
2024-01-11 21:22:55 +0100

    added new tests

--- dnd-inventory-front-end/tests/test.ts
+++ dnd-inventory-front-end/tests/test.ts
@@ -1,6 +1,24 @@
 import { expect, test } from '@playwright/test';
 
-test('index page has expected h1', async ({ page }) => {
-	await page.goto('/');
-	await expect(page.getByRole('heading', { name: 'Welcome to SvelteKit' })).toBeVisible();
+
+
+test.describe.parallel('sessions and session page tests', () => {
+	test('Session table not to be empty', async ({ page }) => {
+		await page.goto('/sessions');
+	
+		const table = await page.innerText('#session-table');
+		expect(table).not.toBe('');
+	});
+	test('inventory table not to be empty', async ({ page }) => {
+		await page.goto('/sessions/3');
+	
+		const table = await page.innerText('#session-table');
+		expect(table).not.toBe('');
+	});
+	test('inventory table not to be empty', async ({ page }) => {
+		await page.goto('/sessions/3');
+	
+		const tab = await page.innerText('#settings-tab');
+		expect(tab).not.toBe('');
+	});
 });
