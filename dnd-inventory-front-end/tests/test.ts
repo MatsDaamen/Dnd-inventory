670e707e51a5db42ba70f8549b29c474a7ac1bce
Mats Daamen <56251011+MatsDaamen@users.noreply.github.com>
2024-01-15 14:56:27 +0100

    check if tests run properly

--- dnd-inventory-front-end/tests/test.ts
+++ dnd-inventory-front-end/tests/test.ts
@@ -21,19 +21,14 @@ test.describe.parallel('sessions and session page tests', () => {
 	test('inventory table not to be empty', async ({ page }) => {
 
 		await page.context().storageState({ path: '$/tests/storageState.json' });
-
-		await page.goto('/sessions/3');
 	
-		const table = await page.getByTestId("inventory");
-		expect(table).not.toBe('');
+		expect(true).toBeTruthy();
 	});
 	test('settings tab is not empty', async ({ page }) => {
 
 		await page.context().storageState({ path: '$/tests/storageState.json' });
-
-		await page.goto('/sessions/3');
 	
 		const tab = await page.getByTitle("members and invite code");
-		expect(tab).not.toBe('');
+		expect(false).toBeTruthy();
 	});
 });
